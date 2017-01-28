862f7181d8f4ed8c9e8047bd9fd2b642b7c785a1
mneli <m.nyeem@elium.academy>
2017-01-28 20:35:06 +0100

    basic email parsing

--- nodeNotifier/private_modules/emailParser/index.js
+++ nodeNotifier/private_modules/emailParser/index.js
@@ -1,3 +1,12 @@
+const path = require('path');
+const fs = require('fs');
+const validator = require('validator');
+
 module.exports = function (data) {
-	console.log(data);
+	// console.log(validator.isEmail(data[0].split(" ")[2].replace("<", " ")));
+	console.log(validator.isEmail(data[0].split(" ")[2].replace("<", "").replace(">", "")));
+	// if (fs.existsSync(data)) {
+	// 	return fs.readFileSync(data).toString();
+	// }
+	// return data;
 };
